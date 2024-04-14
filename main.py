0fa04126ff91c5ad9ec60f8b9acfee92ca619449
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-04-14 09:16:11 +0300

    fignya

--- main.py
+++ main.py
@@ -65,101 +65,101 @@ async def detect(upload_image: UploadFile):
     :return: JSON response containing predicted values
     """
 
-    try:
-        shutil.rmtree(tmp_dir)
-        os.makedirs(tmp_dir)
+    # try:
+    shutil.rmtree(tmp_dir)
+    os.makedirs(tmp_dir)
 
-        # Save uploaded image.
-        hashedFileName = HasherObject.CreateImageFileNameHash(upload_image.filename)
-        async with aiofiles.open((Path() / "Content" / "Predict_Images" / hashedFileName).absolute(),
-                                 'wb') as image_file:
-            await image_file.write(await upload_image.read())
+    # Save uploaded image.
+    hashedFileName = HasherObject.CreateImageFileNameHash(upload_image.filename)
+    async with aiofiles.open((Path() / "Content" / "Predict_Images" / hashedFileName).absolute(),
+                             'wb') as image_file:
+        await image_file.write(await upload_image.read())
 
-        # Log image save success.
-        logger.debug(f"Image saved: {hashedFileName}")
+    # Log image save success.
+    logger.debug(f"Image saved: {hashedFileName}")
 
-        # Make prediction using YOLO model.
-        prediction_class = model.predict((Path() / "Content" / "Predict_Images" / hashedFileName).absolute(), conf=0.6)
+    # Make prediction using YOLO model.
+    prediction_class = model.predict((Path() / "Content" / "Predict_Images" / hashedFileName).absolute(), conf=0.6)
 
-        if prediction_class[0].probs is None:
-            raise HTTPException(status_code=404, detail='Bad Class')
+    if prediction_class[0].probs is None:
+        raise HTTPException(status_code=404, detail='Bad Class')
 
-        # Extract prediction details.
-        confidence = prediction_class[0].probs.top1conf.item()
-        name = prediction_class[0].names[prediction_class[0].probs.top1]
+    # Extract prediction details.
+    confidence = prediction_class[0].probs.top1conf.item()
+    name = prediction_class[0].names[prediction_class[0].probs.top1]
 
-        if name == "vehicle_passport":
-            type = name
-            page_number = 0
-        else:
-            type = name.split("-")[0]
-            page_number = name.split("-")[1]
+    if name == "vehicle_passport":
+        type = name
+        page_number = 0
+    else:
+        type = name.split("-")[0]
+        page_number = name.split("-")[1]
 
-        # Log prediction details.
-        logger.debug(f"Prediction: Type - {type}, Confidence - {confidence:.4f}, Page Number - {page_number}")
+    # Log prediction details.
+    logger.debug(f"Prediction: Type - {type}, Confidence - {confidence:.4f}, Page Number - {page_number}")
 
-        prediction_detect = model2.predict((Path() / "Content" / "Predict_Images" / hashedFileName).absolute(),
-                                           conf=0.5)[0]
+    prediction_detect = model2.predict((Path() / "Content" / "Predict_Images" / hashedFileName).absolute(),
+                                       conf=0.5)[0]
 
-        image = Image.open((Path() / "Content" / "Predict_Images" / hashedFileName).absolute())
-        boxes = prediction_detect.boxes
+    image = Image.open((Path() / "Content" / "Predict_Images" / hashedFileName).absolute())
+    boxes = prediction_detect.boxes
 
-        if boxes is None or len(boxes) == 0:
-            return {
-                "type": type,
-                "confidence": round(confidence, 4),
-                "series": "",
-                "number": "",
-                "page_number": page_number
-            }
-
-        max_box = max(boxes, key=lambda x: float(x.conf[0]))[0]
-
-        cropped_image = image.crop(max_box.xyxy.tolist()[0])
-        # img = np.array(cropped_image)[:, :, ::-1]
-        # if img.shape[0] < img.shape[1]:
-        #     img = img.transpose((2, 0, 1))
-        # else:
-        #     img = img.transpose((2, 1, 0))
-        # img = cv2.resize(img, (250, 50))
-        # print(img.shape)
-        # cropped_image = Image.fromarray(img.transpose(1, 2, 0))
-        cropped_image = cropped_image.resize((250, 50))
-
-        hashedFileName = HasherObject.CreateImageFileNameHash(upload_image.filename)
-        cropped_image_path = os.path.join(tmp_dir, "cropped_" + hashedFileName)
-
-        cropped_image.save(cropped_image_path)
-
-        detected = CRNN.predict(tmp_dir)
-        series = detected[:4]
-        number = detected[4:]
-
-        # Extract additional details based on prediction.
-        if name == "vehicle_passport":
-            type = name
-            page_number = 0
-        else:
-            type = name.split("-")[0]
-            page_number = name.split("-")[1]
-
-        # Log prediction details.
-        logger.debug(f"Prediction: Type - {type}, Confidence - {confidence:.4f}, Page Number - {page_number}")
-
-        # Create dictionary containing all values.
-        all_values = {
+    if boxes is None or len(boxes) == 0:
+        return {
             "type": type,
             "confidence": round(confidence, 4),
-            "series": series,
-            "number": number,
+            "series": "",
+            "number": "",
             "page_number": page_number
         }
-        print(all_values)
-        return JSONResponse(all_values)
+
+    max_box = max(boxes, key=lambda x: float(x.conf[0]))[0]
+
+    cropped_image = image.crop(max_box.xyxy.tolist()[0])
+    # img = np.array(cropped_image)[:, :, ::-1]
+    # if img.shape[0] < img.shape[1]:
+    #     img = img.transpose((2, 0, 1))
+    # else:
+    #     img = img.transpose((2, 1, 0))
+    # img = cv2.resize(img, (250, 50))
+    # print(img.shape)
+    # cropped_image = Image.fromarray(img.transpose(1, 2, 0))
+    cropped_image = cropped_image.resize((250, 50))
+
+    hashedFileName = HasherObject.CreateImageFileNameHash(upload_image.filename)
+    cropped_image_path = os.path.join(tmp_dir, "cropped_" + hashedFileName)
+
+    cropped_image.save(cropped_image_path)
+
+    detected = CRNN.predict(tmp_dir)
+    series = detected[:4]
+    number = detected[4:]
+
+    # Extract additional details based on prediction.
+    if name == "vehicle_passport":
+        type = name
+        page_number = 0
+    else:
+        type = name.split("-")[0]
+        page_number = name.split("-")[1]
+
+    # Log prediction details.
+    logger.debug(f"Prediction: Type - {type}, Confidence - {confidence:.4f}, Page Number - {page_number}")
+
+    # Create dictionary containing all values.
+    all_values = {
+        "type": type,
+        "confidence": round(confidence, 4),
+        "series": series,
+        "number": number,
+        "page_number": page_number
+    }
+    print(all_values)
+    return JSONResponse(all_values)
 
     # Raise exception if there's an IndexError.
-    except IndexError:
-        raise HTTPException(status_code=401, detail='Bad APIToken')
+    # except IndexError:
+    #     raise HTTPException(status_code=401, detail='Bad APIToken')
 
 
 # --------------------------------------------------------------------------
