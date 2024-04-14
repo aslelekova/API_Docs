567eeb042eb143183611298bf936d4a880815cec
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-04-14 16:37:41 +0300

    fix

--- main.py
+++ main.py
@@ -120,7 +120,10 @@ async def detect(upload_image: UploadFile):
 
         # if cropped_image.size[0] < cropped_image.size[1]:
         #     cropped_image = cropped_image.rotate(270, expand=True)
-        cropped_image = cropped_image.resize((250, 50))
+        img_np = np.array(cropped_image)
+        img_resized = cv2.resize(img_np, (250, 50))
+        cropped_image = Image.fromarray(img_resized)
+        # cropped_image = cropped_image.resize((250, 50))
         hashedFileName = HasherObject.CreateImageFileNameHash(upload_image.filename)
         cropped_image_path = os.path.join(tmp_dir, "cropped_" + hashedFileName)
 
